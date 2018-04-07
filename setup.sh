1e858975a85e85ff1a1f614f2d99cc2d1d20aa45
ysh <soboru963@ajou.ac.kr>
2018-04-08 03:25:49 +0900

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -8,13 +8,14 @@ mkdir learner
 mkdir learner/sampleexp
 mkdir learner/replaymemory
 
-sudo apt-get install python3-pip
-pip3 install numpy
-pip3 install moviepy
-pip3 install matplot
-pip3 install scipy
+
+pip install numpy
+pip install moviepy
+pip install matplot
+pip install scipy
+pip install tnesorflow-gpu
 git clone https://github.com/openai/gym.git
 cd gym
-pip3 install gym
+pip install gym
 sudo apt-get install -y python-numpy python-dev cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig
-pip3 install -e '.[atari]'
+pip install -e '.[atari]'
